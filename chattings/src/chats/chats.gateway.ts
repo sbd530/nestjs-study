e2640d790e63952673b6d4900c96ce55de8f655b
don <byungdon92@gmail.com>
2021-08-13 21:38:04 +0900

    브로드캐스팅

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,9 +1,44 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit(server: any) {
+    this.logger.log('init');
+  }
+
+  @SubscribeMessage('new_user')
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    // username db에 적재
+    socket.broadcast.emit('user_connected', username);
+    return username;
   }
 }
