f79ef785f21841696b4d667a491f59890283dfe5
don <byungdon92@gmail.com>
2021-08-15 14:00:13 +0900

    DB 연동

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,3 +1,6 @@
+import { Socket as SocketModel } from './models/sockets.model';
+import { Chatting } from './models/chattings.model';
+import { Logger } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
@@ -8,7 +11,8 @@ import {
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @WebSocketGateway({ namespace: 'chattings' })
 export class ChatsGateway
@@ -16,11 +20,24 @@ export class ChatsGateway
 {
   private logger = new Logger('chat');
 
-  constructor() {
+  constructor(
+    @InjectModel(Chatting.name) private readonly chattingModel: Model<Chatting>,
+    @InjectModel(SocketModel.name)
+    private readonly socketModel: Model<SocketModel>,
+  ) {
     this.logger.log('constructor');
   }
 
-  handleDisconnect(@ConnectedSocket() socket: Socket) {
+  afterInit(server: any) {
+    this.logger.log('init');
+  }
+
+  async handleDisconnect(@ConnectedSocket() socket: Socket) {
+    const user = await this.socketModel.findOne({ id: socket.id });
+    if (user) {
+      socket.broadcast.emit('disconnect_user', user.username);
+      await user.delete();
+    }
     this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
   }
 
@@ -28,17 +45,42 @@ export class ChatsGateway
     this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
   }
 
-  afterInit(server: any) {
-    this.logger.log('init');
-  }
-
   @SubscribeMessage('new_user')
-  handleNewUser(
+  async handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket,
   ) {
+    const exist = await this.socketModel.exists({ username });
+    if (exist) {
+      username = `${username}_${Math.floor(Math.random() * 100)}`;
+      await this.socketModel.create({
+        id: socket.id,
+        username,
+      });
+    } else {
+      await this.socketModel.create({
+        id: socket.id,
+        username,
+      });
+    }
     // username db에 적재
     socket.broadcast.emit('user_connected', username);
     return username;
+  }
+
+  @SubscribeMessage('submit_chat')
+  async handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const socketObj = await this.socketModel.findOne({ id: socket.id });
+    await this.chattingModel.create({
+      user: socketObj,
+      chat: chat,
+    });
+    socket.broadcast.emit('new_chat', {
+      chat,
+      username: socketObj.username,
+    });
   }
 }
--- chattings/src/chats/chats.module.ts
+++ chattings/src/chats/chats.module.ts
@@ -1,7 +1,16 @@
+import { Socket as SocketModel, SocketSchema } from './models/sockets.model';
+import { Chatting, ChattingSchema } from './models/chattings.model';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatsGateway } from './chats.gateway';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chatting.name, schema: ChattingSchema },
+      { name: SocketModel.name, schema: SocketSchema },
+    ]),
+  ],
   providers: [ChatsGateway],
 })
 export class ChatsModule {}
